69c5c6d63414b76e439e8313985965c04c829fdb
Tim Corley <tcorley215@gmail.com>
2022-03-28 15:40:46 -0400

    adding jobs endpoint - get topic stats

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,8 +1,10 @@
+from urllib import request
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Avg, Min, Max, Count
 from .serializer import JobSerializer
 from .models import Job
 
@@ -47,3 +49,19 @@ def deleteJob(request, pk):
     job = get_object_or_404(Job, id=pk)
     job.delete()
     return Response({ 'message': 'Job has successfully been deleted.' }, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def getTopicStats(request, topic):
+    args = { 'title__icontains': topic }
+    jobs = Job.objects.filter(**args)
+    if len(jobs) == 0:
+        return Response({ 'message': 'No stats found for {topic}'.format(topic=topic) })
+    stats = jobs.aggregate(
+        total_jobs = Count('title'),
+        avg_positions = Avg('positions'),
+        avg_salary = Avg('salary'),
+        min_salary = Min('salary'),
+        max_salary = Max('salary'),
+    )
+
+    return Response(stats)
