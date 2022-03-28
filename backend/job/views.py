8a6d096745e0e3ca8a705eee51514476b9ee1e4b
Tim Corley <tcorley215@gmail.com>
2022-03-28 16:25:38 -0400

    adding pagination to jobs endpoint

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,8 +1,10 @@
 from urllib import request
+from warnings import filters
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from django.db.models import Avg, Min, Max, Count
 from .serializer import JobSerializer
@@ -11,26 +13,47 @@ from .models import Job
 
 @api_view(['GET'])
 def getAllJobs(request):
+    
     filterset = JobsFilter(request.GET, queryset=Job.objects.all().order_by('id'))
-    serializer = JobSerializer(filterset.qs, many=True)
-    return Response(serializer.data)
+    
+    count = filterset.qs.count()
+
+    # PAGINATION
+    resPerPage = 3
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
+    serializer = JobSerializer(queryset, many=True)
+    
+    return Response({
+        'count': count,
+        'resPerPage': resPerPage,
+        'jobs': serializer.data
+    })
 
 @api_view(['GET'])
 def getJob(request, pk):
     job = get_object_or_404(Job, id=pk)
+    
     serializer = JobSerializer(job, many=False)
+    
     return Response(serializer.data)
 
 @api_view(['POST'])
 def newJob(request):
     data = request.data
     job = Job.objects.create(**data)
+    
     serializer = JobSerializer(job, many=False)
+    
     return Response(serializer.data)
 
 @api_view(['PUT'])
 def updateJob(request, pk):
     job = get_object_or_404(Job, id=pk)
+    
     job.title = request.data['title']
     job.description = request.data['description']
     job.email = request.data['email']
@@ -41,22 +64,30 @@ def updateJob(request, pk):
     job.salary = request.data['salary']
     job.positions = request.data['positions']
     job.company = request.data['company']
+    
     job.save()
+    
     serializer = JobSerializer(job, many=False)
+    
     return Response(serializer.data)
 
 @api_view(['DELETE'])
 def deleteJob(request, pk):
     job = get_object_or_404(Job, id=pk)
+    
     job.delete()
+    
     return Response({ 'message': 'Job has successfully been deleted.' }, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def getTopicStats(request, topic):
     args = { 'title__icontains': topic }
+    
     jobs = Job.objects.filter(**args)
+    
     if len(jobs) == 0:
         return Response({ 'message': 'No stats found for {topic}'.format(topic=topic) })
+    
     stats = jobs.aggregate(
         total_jobs = Count('title'),
         avg_positions = Avg('positions'),
