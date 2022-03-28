91ee1e68234803eb100c57a66411dc0f20eb0d4d
Tim Corley <tcorley215@gmail.com>
2022-03-28 14:57:18 -0400

    adding job endpoints - get, create, update

---
+++ backend/job/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('jobs/', views.getAllJobs, name='jobs'),
+    path('jobs/new/', views.newJob, name='new_job'),
+    path('jobs/<str:pk>/', views.getJob, name='job'),
+    path('jobs/<str:pk>/update/', views.updateJob, name='update_job')
+
+]
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import JobSerializer
+from .models import Job
 
-# Create your views here.
+@api_view(['GET'])
+def getAllJobs(request):
+    jobs = Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def newJob(request):
+    data = request.data
+    job = Job.objects.create(**data)
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+    job.save()
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
