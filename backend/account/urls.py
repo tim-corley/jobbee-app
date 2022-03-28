1f860c743fbfd269c634c8cd942c37e2ea8d2d61
Tim Corley <tcorley215@gmail.com>
2022-03-28 19:09:34 -0400

    creating update user view

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('me/', views.currentUser, name='current_user')
+    path('me/', views.currentUser, name='current_user'),
+    path('me/update/', views.updateUser, name='update_user')
 ]
