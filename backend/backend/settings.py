c5280aee9ede53526ed032392ea1502be7bacf25
Tim Corley <tcorley215@gmail.com>
2022-03-30 15:38:51 -0400

    fixing cst exception handler
    adding uploadResume endpoint
    
    adding AWS creds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
-    # 'storages'
+    'storages',
     'django.contrib.gis',
     'django_filters',
 
@@ -154,3 +154,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AWS Configurations
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
